a91b4bc576c895a8f24e2fcd77af10c4b552ec30
NailLegProcessorDivide <53022692+NailLegProcessorDivide@users.noreply.github.com>
2020-02-20 20:53:44 +0000

    Update LibraryManager.java

--- eclipseproj/hascodeSolution/src/LibraryManager.java
+++ eclipseproj/hascodeSolution/src/LibraryManager.java
@@ -41,4 +41,14 @@ public class LibraryManager {
 		});
 		return alllibs;
 	}
+	
+	public static Library[] heuristic2() {
+		Library[] alllibs = Main.libs;
+		Arrays.sort(alllibs, new Comparator<Library>() {
+			public int compare(Library l1, Library l2) {
+				return Double.compare(l2.scorePotential/(double)l2.signup/l2.signup,  l1.scorePotential/(double)l1.signup/l2.signup);
+			}
+		});
+		return alllibs;
+	}
 }
