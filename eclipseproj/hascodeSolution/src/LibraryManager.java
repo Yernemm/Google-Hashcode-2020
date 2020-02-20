5ed9b12ee601d8a5e1983b6560508aa1caaed441
Yernemm <yernemm@yernemm.xyz>
2020-02-20 19:46:19 +0000

    Update LibraryManager.java

--- eclipseproj/hascodeSolution/src/LibraryManager.java
+++ eclipseproj/hascodeSolution/src/LibraryManager.java
@@ -1,4 +1,17 @@
 
 public class LibraryManager {
 
+	LibraryManager(){
+		
+	}
+	
+	Integer[][] bruteForceOrders(int length) {
+		
+		return null; //to-do
+	}
+	
+	Integer[] bestScorePotential() {
+		
+		return null; //to-do
+	}
 }
