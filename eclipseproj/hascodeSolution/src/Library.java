6b140975eeb07bdfa72e3489edce068dd53f6791
Yernemm <yernemm@yernemm.xyz>
2020-02-20 18:42:42 +0000

    Update Library.java

--- eclipseproj/hascodeSolution/src/Library.java
+++ eclipseproj/hascodeSolution/src/Library.java
@@ -1,15 +1,47 @@
-
+import java.util.ArrayList;
 public class Library {
 	
 	public int[] books;
 	public int signup;
 	public int scanrate;
+	public ArrayList<Integer> scanned;
+	private int bookOn;
+	public boolean done;
 	
 	public Library(int[] books, int signup, int scanrate)
 	{
 		this.books = books;
 		this.signup = signup;
 		this.scanrate = scanrate;
+		this.scanned = new ArrayList<Integer>();
+		bookOn = 0;
+		done = false;
+	}
+	
+	public int[] tick()
+	{
+		if(!this.done) {
+			for (int i = 0; i < scanrate; i++) {
+				if(i + bookOn < books.length) {
+					scanned.add(books[i]);
+					
+				}
+				
+			}
+			
+			bookOn += scanrate;
+			
+			if(bookOn >= books.length -1) {
+				done = true;
+			}
+			
+			
+		}
+
+		
+
+		
+		return scanned;
 	}
 
 }
