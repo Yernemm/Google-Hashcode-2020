7d93268cf74b6af5ba0e578944ebdaa998ec5cb6
NailLegProcessorDivide <53022692+NailLegProcessorDivide@users.noreply.github.com>
2020-02-20 20:05:21 +0000

    sort books

--- eclipseproj/hascodeSolution/src/Main.java
+++ eclipseproj/hascodeSolution/src/Main.java
@@ -15,14 +15,15 @@ public class Main {
 	public static Library setup;
 	public static ArrayList<Library> signedLibs = new ArrayList<Library>();
 	public static int score = 0;
+	public static int maxScore = 0;
 	public static void main(String[] args) {
 		
 		try {
 			//Scanner sc = new Scanner(new File("../../data/a_example.txt"));
-			//Scanner sc = new Scanner(new File("../../data/b_read_on.txt"));
+			Scanner sc = new Scanner(new File("../../data/b_read_on.txt"));
 			//Scanner sc = new Scanner(new File("../../data/c_incunabula.txt"));
 			//Scanner sc = new Scanner(new File("../../data/d_tough_choices.txt"));
-			Scanner sc = new Scanner(new File("../../data/e_so_many_books.txt"));
+			//Scanner sc = new Scanner(new File("../../data/e_so_many_books.txt"));
 			//Scanner sc = new Scanner(new File("../../data/f_libraries_of_the_world.txt"));
 			nBooks = sc.nextInt();
 			nLibs = sc.nextInt();
@@ -42,46 +43,20 @@ public class Main {
 				for(int j = 0; j < nLibBooks; j++) {
 					libBooks[j] = sc.nextInt();
 				}
+				for(int j = 0; j < nLibBooks; j++) {
+					for(int k = 1; k < nLibBooks-j; k++) {
+						if(bookScores[libBooks[k-1]] < bookScores[libBooks[k]]) {
+							int t = libBooks[k-1];
+							libBooks[k-1] = libBooks[k];
+							libBooks[k] = t;
+						}
+					}
+				}
 				libs[i] = new Library(libBooks, setupTime, booksPerDay, i);
 			}
 			
-			int ind = 0;
-			setup = libs[ind++];
-			boolean allset = false;
-			for(int d = 0; d < nDays; d++) {
-				for(Library l : signedLibs) {
-					score += l.tick();
-				}
-				if(!allset && setup.setup()) {
-					signedLibs.add(setup);
-					if(ind < libs.length) {
-						setup = libs[ind++];
-					}
-					else {
-						allset = true;
-					}
-				}
-			}
 			
-			
-			PrintStream ps = new PrintStream(new File("sube.txt"));
-			int n = 0;
-			for(Library l : signedLibs) {
-				if(l.scanned.size()>0) {
-					n++;
-				}
-			}
-			ps.println(n);
-			for(Library l : signedLibs) {
-				if(l.scanned.size()>0) {
-					ps.println(l.id + " " + l.scanned.size());
-					for(int i : l.scanned) {
-						ps.print(i + " ");
-					}
-					ps.println();
-				}
-			}
-			
+			run();
 			
 			
 			
@@ -91,9 +66,66 @@ public class Main {
 		}
 		
 	}
+	
+	public static int run() {
+		score = 0;
+		reset();
+		int ind = 0;
+		setup = libs[ind++];
+		boolean allset = false;
+		for(int d = 0; d < nDays; d++) {
+			for(Library l : signedLibs) {
+				score += l.tick();
+			}
+			if(!allset && setup.setup()) {
+				signedLibs.add(setup);
+				if(ind < libs.length) {
+					setup = libs[ind++];
+				}
+				else {
+					allset = true;
+				}
+			}
+		}
+		
+
+		if(maxScore < score) {
+			maxScore = score;
+			try {
+				PrintStream ps = new PrintStream(new File("sube.txt"));
+				int n = 0;
+				for(Library l : signedLibs) {
+					if(l.scanned.size()>0) {
+						n++;
+					}
+				}
+				ps.println(n);
+				for(Library l : signedLibs) {
+					if(l.scanned.size()>0) {
+						ps.println(l.id + " " + l.scanned.size());
+						for(int i : l.scanned) {
+							ps.print(i + " ");
+						}
+						ps.println();
+					}
+				}
+				System.out.println("found new!!");
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return score;
+	}
+	
 	public static void reset() {
 		for(Library l : libs) {
 			l.reset();
+		}
+		signedLibs = new ArrayList<Library>();
+		//score = 0;
+		for(int i = 0; i < nBooks; i++) {
+			bookScanned[i] = false;
 		}
 	}
 
