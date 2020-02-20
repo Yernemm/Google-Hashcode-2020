240700c7dd60a9b12329bc30ec4e2b408f2ed5ad
Yernemm <yernemm@yernemm.xyz>
2020-02-20 18:05:48 +0000

    Create Library.java

---
+++ eclipseproj/hascodeSolution/src/Library.java
@@ -0,0 +1,15 @@
+
+public class Library {
+	
+	public int[] books;
+	public int signup;
+	public int scanrate;
+	
+	public Library(int[] books, int signup, int scanrate)
+	{
+		this.books = books;
+		this.signup = signup;
+		this.scanrate = scanrate;
+	}
+
+}
