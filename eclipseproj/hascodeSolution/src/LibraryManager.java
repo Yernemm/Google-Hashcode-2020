8f3eca5f8ab03d3a89b2a602b1b2c8a553148232
Yernemm <yernemm@yernemm.xyz>
2020-02-20 20:02:29 +0000

    bestscorepotential

--- eclipseproj/hascodeSolution/src/LibraryManager.java
+++ eclipseproj/hascodeSolution/src/LibraryManager.java
@@ -1,4 +1,5 @@
-
+import java.util.Arrays;
+import java.util.Comparator;
 public class LibraryManager {
 
 	LibraryManager(){
@@ -10,8 +11,13 @@ public class LibraryManager {
 		return null; //to-do
 	}
 	
-	Integer[] bestScorePotential() {
-		
-		return null; //to-do
+	Library[] bestScorePotential() {
+		Library[] alllibs = Main.libs;
+		Arrays.sort(alllibs, new Comparator<Library>() {
+			public int compare(Library l1, Library l2) {
+				return Integer.compare(l2.scorePotential, l1.scorePotential);
+			}
+		});
+		return alllibs;
 	}
 }
